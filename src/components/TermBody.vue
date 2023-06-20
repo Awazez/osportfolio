b8e99396b5528b02f3ccb50604f47ec77f4746a1
cherbourg@fub.fr <cherbourg@fub.fr>
2023-06-20 14:49:39 +0200

    🎉 Init TermBody.vue

---
+++ src/components/TermBody.vue
@@ -0,0 +1,28 @@
+<template>
+    
+    <div class="inner-box">
+              
+              <div class="text-box">
+             
+                <TermWelcome/>
+                <TermPrompt/>
+               
+              </div>
+            </div>
+</template>
+
+<style scoped>
+
+.inner-box {
+      width: 640px;
+      height: 275px;
+      color:white;
+      background-color: #201353;
+    }
+
+.text-box {
+    padding: 10px;
+   }
+
+
+</style>
