b6b874395fb1d46104762e66c5f6e6343bb14554
cherbourg@fub.fr <cherbourg@fub.fr>
2023-06-20 14:52:19 +0200

    🎉 Init TermPrompt.vue

---
+++ src/components/Common/TermPrompt.vue
@@ -0,0 +1,35 @@
+<template>
+ <section class="terminalBody">          
+            <div class="terminalPrompt">            
+              <span class="terminalPromptUser">awazez@mac-mini-de-Martin ~ %</span>                               
+              <span class="terminalPromptCursor"></span> 
+              <input>
+              
+            </div>        
+          </section>
+
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+
+.terminalPrompt {  
+  display: flex;
+}
+.terminalPromptUser {  
+  font-size: 14px;
+  font-weight: bold;
+  font-family: 'Poppins', sans-serif;
+  color:#d1059b;
+}
+
+.terminalPromptCursor {  
+  display: block;  
+  height: 17px;  
+  width: 8px;  
+  margin-left: 9px;  
+  animation: blink 1200ms linear infinite;
+
+}
+
+</style>
