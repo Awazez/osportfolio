8ba79d29a9eeea8c56adde697ca8e8db68fceacd
cherbourg@fub.fr <cherbourg@fub.fr>
2023-06-20 14:57:55 +0200

    🎉 Init TermApp.vue

---
+++ views/TermApp.vue
@@ -0,0 +1,29 @@
+<script setup>
+import TermContainer from './TermContainer.vue';
+import TermIcon from "../src/components/TermIcon.vue"
+
+</script>
+
+
+<template>
+    <div class="backgroundImage">
+        <TermContainer/>
+        <TermIcon/>
+</div>
+</template>
+
+<style scoped>
+
+.backgroundImage {
+  background-image: url("../src/assets/bg-ground.jpg");
+  font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+  position: relative;
+  min-width: 320px;
+  min-height: 100vh;
+  background-size: cover;
+  color: black;
+  overflow: hidden;
+}
+
+
+</style>
