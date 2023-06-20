e23933988034bed23aa6b22f73edd31ce1dea5f3
cherbourg@fub.fr <cherbourg@fub.fr>
2023-06-20 14:52:05 +0200

    🎉 Init Termicon.vue

---
+++ src/components/TermIcon.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+</script>
+
+<template>
+      <div class="container">
+        <img @click="$emit('someEvent')" src="../assets/terminal.png">
+    <span>Terminal</span>
+    </div>
+</template>
+
+<style scoped>
+
+span {
+    color:white;
+    margin-left: -62px;
+}
+
+img {
+  width: 60px;
+  height: 60px;
+  margin-bottom: 8px;
+}
+.container {
+    margin-left: 50px;
+    margin-top: 15px;
+}
+
+</style>
